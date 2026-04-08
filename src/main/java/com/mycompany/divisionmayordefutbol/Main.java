e4c72815d380d8d7fde83ca3dbfd7d4bfe6e2c71
RYZEN <yael.botero@uao.edu.co>
2026-04-07 20:27:04 -0500

    Entrada del main

--- src/main/java/com/mycompany/divisionmayordefutbol/Main.java
+++ src/main/java/com/mycompany/divisionmayordefutbol/Main.java
@@ -8,9 +8,33 @@ package com.mycompany.divisionmayordefutbol;
  *
  * @author RYZEN
  */
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        DiamayorController ctrl = new DiamayorController();
+        DiamayorView view = new DiamayorView();
+        Scanner sc = new Scanner(System.in);
+
+        ctrl.agregar(new EquipoDimayor("América de Cali",       "Cali",         10, 3, 2));
+        ctrl.agregar(new EquipoDimayor("Atlético Nacional",     "Medellín",     10, 3, 2));
+        ctrl.agregar(new EquipoDimayor("Millonarios FC",        "Bogotá",        9, 2, 4));
+        ctrl.agregar(new EquipoDimayor("Junior FC",             "Barranquilla",  7, 5, 3));
+        ctrl.agregar(new EquipoDimayor("Deportivo Cali",        "Cali",          8, 4, 3));
+
+        int op;
+        do {
+            view.mostrarMenu();
+            op = sc.nextInt(); sc.nextLine();
+            switch (op) {
+                case 1: view.mostrarLista(ctrl.getEquipos()); break;
+                case 2: view.mostrarPuntaje(ctrl.getMayorPuntaje()); break;
+                case 3:
+                    System.out.print("Nombre: ");
+                    view.mostrarEquipo(ctrl.buscar(sc.nextLine())); break;
+                case 4: view.mostrarLideres(ctrl.getLideres(), ctrl.getMayorPuntaje()); break;
+                case 0: System.out.println("¡Hasta luego!"); break;
+            }
+        } while (op != 0);
     }
 }
